a8307185845ff67a5d7fb9a0da2e8d625d5cd7f3
cihancalli <cihan.callii@gmail.com>
2022-05-04 00:16:37 +0300

    Retrofit and RxJava Integration

--- app/src/main/java/com/zerdasoftware/cryptocurrencyprices/MainActivity.kt
+++ app/src/main/java/com/zerdasoftware/cryptocurrencyprices/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val API_KEY = ""
+        val API_SERVER_URL = "https://api.nomics.com/v1/currencies/ticker?key="
     }
 }
