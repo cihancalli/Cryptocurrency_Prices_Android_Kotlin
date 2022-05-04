8f0b2fa3b06319a8a28e11af30ff410cc250c15b
cihancalli <cihan.callii@gmail.com>
2022-05-04 14:14:52 +0300

    RxJava and Composite

--- app/src/main/java/com/zerdasoftware/cryptocurrencyprices/service/CryptoAPI.kt
+++ app/src/main/java/com/zerdasoftware/cryptocurrencyprices/service/CryptoAPI.kt
@@ -1,6 +1,7 @@
 package com.zerdasoftware.cryptocurrencyprices.service
 
 import com.zerdasoftware.cryptocurrencyprices.model.CyrptoModel
+import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 import java.util.*
@@ -9,7 +10,8 @@ interface CryptoAPI {
 
     var  timer: Timer
     //GET, POST, UPDATE, DELETE
-    @GET("currencies/ticker?key=7479b81071e3e26ace906db8ebb1d57c99460928")
-    fun GetData() : Call<List<CyrptoModel>>
+    @GET("currencies/ticker?key=YOUR_KEY_HERE")
+    //fun GetData() : Call<List<CyrptoModel>>
+    fun GetData() :Observable<List<CyrptoModel>>
 
 }
--- app/src/main/java/com/zerdasoftware/cryptocurrencyprices/view/MainActivity.kt
+++ app/src/main/java/com/zerdasoftware/cryptocurrencyprices/view/MainActivity.kt
@@ -8,11 +8,15 @@ import com.zerdasoftware.cryptocurrencyprices.R
 import com.zerdasoftware.cryptocurrencyprices.adapter.RecyclerViewAdapter
 import com.zerdasoftware.cryptocurrencyprices.model.CyrptoModel
 import com.zerdasoftware.cryptocurrencyprices.service.CryptoAPI
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Listener {
@@ -23,9 +27,14 @@ class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Listener {
     private lateinit var cyrptoModels:ArrayList<CyrptoModel>
     private var recyclerViewAdapter : RecyclerViewAdapter? = null
 
+    //Disposable
+    private var compositeDisposable:CompositeDisposable? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        compositeDisposable = CompositeDisposable()
 
         //RecyclerViewAdapter
 
@@ -40,11 +49,19 @@ class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Listener {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
-            .build()
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build().create(CryptoAPI::class.java)
 
+
+        compositeDisposable?.add(retrofit.GetData()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::handleResponse)
+        )
+
+        /*
         val service = retrofit.create(CryptoAPI::class.java)
         val call = service.GetData()
-
         call.enqueue(object: Callback<List<CyrptoModel>>{
             override fun onResponse(
                 call: Call<List<CyrptoModel>>,
@@ -58,21 +75,30 @@ class MainActivity : AppCompatActivity(), RecyclerViewAdapter.Listener {
                             recyclerViewAdapter = RecyclerViewAdapter(it,this@MainActivity)
                             rv.adapter = recyclerViewAdapter
                         }
-
-                        /*
                         for (cyrptoModel:CyrptoModel in cyrptoModels){
                             println("${cyrptoModel.currency}")
                             println("${cyrptoModel.price}")
                         }
-                         */
                     }
                 }
             }
-
             override fun onFailure(call: Call<List<CyrptoModel>>, t: Throwable) {
                 t.printStackTrace()
             }
-        })
+        })*/
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable?.clear()
+    }
+
+    private fun handleResponse(cyrptoList : List<CyrptoModel>){
+        cyrptoModels = ArrayList(cyrptoList)
+        cyrptoModels?.let {
+            recyclerViewAdapter = RecyclerViewAdapter(it,this@MainActivity)
+            rv.adapter = recyclerViewAdapter
+        }
     }
 
     override fun onItemClick(cyrptoModel: CyrptoModel) {
